9cd99e9eb6f13548344e12ea5345c1fb68d1fdd9
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-05-04 11:35:05 -0400

    chore(release): v2.0.0-rc.0 [skip ci]

--- auto.config.ts
+++ auto.config.ts
@@ -1,14 +1,9 @@
 import { AutoRc } from 'auto';
 import { INpmConfig } from '@auto-it/npm';
-import { IExecPluginOptions } from '@auto-it/exec';
 
 const npmOptions: INpmConfig = {
   setRcToken: false,
   publishFolder: 'dist/forge-react'
-};
-
-const execOptions: IExecPluginOptions = {
-  afterVersion: `cd ${npmOptions.publishFolder} && npm version $npm_package_version --no-git-tag-version --no-commit-hooks --ignore-scripts`
 };
 
 export default function rc(): AutoRc {
@@ -16,9 +11,7 @@ export default function rc(): AutoRc {
     versionBranches: true,
     prereleaseBranches: ['dev', 'beta', 'rc', 'next'],
     author: 'GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>',
-    canary: { target: 'pr-body' },
     plugins: [
-      ['exec', execOptions],
       ['npm', npmOptions],
       'conventional-commits',
       'released',
