fbdc7f476a11bd3ff2eee7164e775cd5ac3fab5c
Kieran Nichols <kieran.nichols@tylertech.com>
2022-06-25 10:33:52 -0400

    feat: oss prep (#1)

--- src/projects/forge-react/src/hooks/useForgeBusyIndicator.tsx
+++ src/projects/forge-react/src/hooks/useForgeBusyIndicator.tsx
@@ -1,10 +1,11 @@
-import { BusyIndicatorLayoutDirection, IBusyIndicatorComponent } from '@tylertech/forge';
+import { BUSY_INDICATOR_CONSTANTS, IBusyIndicatorComponent } from '@tylertech/forge';
 import { useDynamicForgeComponent, UseDynamicComponentDelegate } from './useDynamicForgeComponent';
 
 export interface ForgeBusyIndicatorOptions extends Partial<IBusyIndicatorComponent> {
   busyIndicatorClass?: string;
   busyIndicatorAttributes?: Map<string, string>;
   hostElement?: HTMLElement;
+  cancelCallback?: () => void;
 }
 
 export type UseForgeBusyIndicatorResult = [
@@ -18,7 +19,7 @@ export const useForgeBusyIndicator = (): UseForgeBusyIndicatorResult => {
   };
   const controller = useDynamicForgeComponent('forge-busy-indicator', undefined, undefined, delegate);
 
-  function show({ busyIndicatorClass, hostElement, busyIndicatorAttributes, ...options }: ForgeBusyIndicatorOptions = {}): IBusyIndicatorComponent {
+  function show({ busyIndicatorClass, hostElement, busyIndicatorAttributes, cancelCallback, ...options }: ForgeBusyIndicatorOptions = {}): IBusyIndicatorComponent {
     const busyIndicatorElement = controller.show(options);
 
     if (busyIndicatorAttributes) {
@@ -27,6 +28,10 @@ export const useForgeBusyIndicator = (): UseForgeBusyIndicatorResult => {
 
     if (busyIndicatorClass) {
       busyIndicatorElement.classList.add(busyIndicatorClass);
+    }
+
+    if (typeof cancelCallback === 'function') {
+      busyIndicatorElement.addEventListener(BUSY_INDICATOR_CONSTANTS.events.CANCEL, () => cancelCallback());
     }
     
     return busyIndicatorElement;
