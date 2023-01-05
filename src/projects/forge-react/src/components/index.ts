9aaf6d29bcae6ba3e41785c237c28af9ca3b61a0
Kieran Nichols <kieran.nichols@tylertech.com>
2023-01-05 11:37:25 -0500

    [next][dialog] static usage (#4)
    feat(dialog): support static usage
    
    BREAKING CHANGE: the <ForgeDialog> component is now just a standard element proxy for statically using the underlying <forge-dialog> element. Use the hook to dynamically use the dialog component if desired

--- src/projects/forge-react/src/components/index.ts
+++ src/projects/forge-react/src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './component-wrappers';
 export * from './ForgeBottomSheet';
 export * from './ForgeBusyIndicator';
-export * from './ForgeDialog';
 export * from './ForgePopup';
 export * from './ForgeToast';
