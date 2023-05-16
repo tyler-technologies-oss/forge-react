e34089f12f90dd32ff84634bbfcec0d7b3a473c7
nickonometry <nick.andrews@tylertech.com>
2023-05-16 16:11:46 -0400

    chore: removed unused code from the demo

--- src/demos/stack/StackDemo.tsx
+++ src/demos/stack/StackDemo.tsx
@@ -1,14 +1,7 @@
-import { IconRegistry } from '@tylertech/forge';
 import { ForgeStack } from 'src/projects/forge-react/src';
-import { tylIconAddAlert } from '@tylertech/tyler-icons/standard';
-import { useEffect } from 'react';
 import './StackDemo.scss';
 
 export function StackDemo(): JSX.Element {
-  useEffect(() => {
-    IconRegistry.define(tylIconAddAlert);
-  }, []);
-
   return (
     <ForgeStack gap="32">
       <h2 className="forge-typography--title">Stack demos</h2>
