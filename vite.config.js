5239cd895b26ba51a7d2194d3950be580048dc21
Kieran Nichols <kieran.nichols@tylertech.com>
2024-05-16 14:27:26 -0400

    feat: support forge 3.0 (#11)

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => ({
+  build: {
+    outDir: 'build'
+  },
+  plugins: [
+    tsconfigPaths(),
+    react()
+  ]
+}));
+
