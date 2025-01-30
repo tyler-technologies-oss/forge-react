61c495c604b5e533a43d6bbf5e32fe6583bcb7c0
Sam Richardson <sam.richardson@tylertech.com>
2025-01-30 13:12:32 -0500

    chore: update to support forge 3.5

--- lib/index.ts
+++ lib/index.ts
@@ -77,14 +77,21 @@ import {
   IToolbarComponent,
   ITooltipComponent,
   IViewComponent,
-  IViewSwitcherComponent
+  IViewSwitcherComponent,
+  KeyComponent,
+  KeyItemComponent,
+  MeterComponent,
+  MeterGroupComponent
 } from '@tylertech/forge';
 
 export * from './components';
 export * from './hooks';
 export * from './utils';
 
-export type HTMLElementProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+export type HTMLElementProps = React.DetailedHTMLProps<
+React.HTMLAttributes<HTMLElement>,
+HTMLElement
+>;
 export type CustomElementProps<T> = HTMLElementProps & Partial<T>;
 
 declare global {
@@ -92,22 +99,22 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       'forge-accordion': CustomElementProps<IAccordionComponent>;
-      'forge-app-bar': CustomElementProps<IAppBarComponent>;
       'forge-app-bar-help-button': CustomElementProps<IAppBarHelpButtonComponent>;
       'forge-app-bar-menu-button': CustomElementProps<IAppBarMenuButtonComponent>;
       'forge-app-bar-notification-button': CustomElementProps<IAppBarNotificationButtonComponent>;
       'forge-app-bar-profile-button': CustomElementProps<IAppBarProfileButtonComponent>;
       'forge-app-bar-search': CustomElementProps<IAppBarSearchComponent>;
+      'forge-app-bar': CustomElementProps<IAppBarComponent>;
       'forge-autocomplete': CustomElementProps<IAutocompleteComponent>;
       'forge-avatar': CustomElementProps<IAvatarComponent>;
       'forge-backdrop': CustomElementProps<IBackdropComponent>;
       'forge-badge': CustomElementProps<IBadgeComponent>;
       'forge-banner': CustomElementProps<IBannerComponent>;
       'forge-bottom-sheet': CustomElementProps<IBottomSheetComponent>;
-      'forge-button': CustomElementProps<IButtonComponent>;
       'forge-button-area': CustomElementProps<IButtonAreaComponent>;
-      'forge-button-toggle': CustomElementProps<IButtonToggleComponent>;
       'forge-button-toggle-group': CustomElementProps<IButtonToggleGroupComponent>;
+      'forge-button-toggle': CustomElementProps<IButtonToggleComponent>;
+      'forge-button': CustomElementProps<IButtonComponent>;
       'forge-calendar': CustomElementProps<ICalendarComponent>;
       'forge-card': CustomElementProps<ICardComponent>;
       'forge-checkbox': CustomElementProps<ICheckboxComponent>;
@@ -123,25 +130,29 @@ declare global {
       'forge-dialog': CustomElementProps<IDialogComponent>;
       'forge-divider': CustomElementProps<IDividerComponent>;
       'forge-drawer': CustomElementProps<IDrawerComponent>;
-      'forge-mini-drawer': CustomElementProps<IMiniDrawerComponent>;
-      'forge-modal-drawer': CustomElementProps<IModalDrawerComponent>;
       'forge-expansion-panel': CustomElementProps<IExpansionPanelComponent>;
+      'forge-fab': CustomElementProps<IFloatingActionButtonComponent>;
       'forge-field': CustomElementProps<IFieldComponent>;
       'forge-file-picker': CustomElementProps<IFilePickerComponent>;
-      'forge-fab': CustomElementProps<IFloatingActionButtonComponent>;
       'forge-focus-indicator': CustomElementProps<IFocusIndicatorComponent>;
-      'forge-icon': CustomElementProps<IIconComponent>;
       'forge-icon-button': CustomElementProps<IIconButtonComponent>;
+      'forge-icon': CustomElementProps<IIconComponent>;
       'forge-inline-message': CustomElementProps<IInlineMessageComponent>;
+      'forge-key-item': CustomElementProps<KeyItemComponent>;
+      'forge-key': CustomElementProps<KeyComponent>;
       'forge-keyboard-shortcut': CustomElementProps<IKeyboardShortcutComponent>;
       'forge-label-value': CustomElementProps<ILabelValueComponent>;
       'forge-linear-progress': CustomElementProps<ILinearProgressComponent>;
-      'forge-list': CustomElementProps<IListComponent>;
       'forge-list-item': CustomElementProps<IListItemComponent>;
+      'forge-list': CustomElementProps<IListComponent>;
       'forge-menu': CustomElementProps<IMenuComponent>;
+      'forge-meter-group': CustomElementProps<MeterGroupComponent>;
+      'forge-meter': CustomElementProps<MeterComponent>;
+      'forge-mini-drawer': CustomElementProps<IMiniDrawerComponent>;
+      'forge-modal-drawer': CustomElementProps<IModalDrawerComponent>;
       'forge-open-icon': CustomElementProps<IOpenIconComponent>;
-      'forge-option': CustomElementProps<IOptionComponent>;
       'forge-option-group': CustomElementProps<IOptionGroupComponent>;
+      'forge-option': CustomElementProps<IOptionComponent>;
       'forge-overlay': CustomElementProps<IOverlayComponent>;
       'forge-page-state': CustomElementProps<IPageStateComponent>;
       'forge-paginator': CustomElementProps<IPaginatorComponent>;
@@ -149,27 +160,27 @@ declare global {
       'forge-profile-card': CustomElementProps<IProfileCardComponent>;
       'forge-radio': CustomElementProps<IRadioComponent>;
       'forge-scaffold': CustomElementProps<IScaffoldComponent>;
-      'forge-select': CustomElementProps<ISelectComponent>;
       'forge-select-dropdown': CustomElementProps<ISelectDropdownComponent>;
+      'forge-select': CustomElementProps<ISelectComponent>;
       'forge-skeleton': CustomElementProps<ISkeletonComponent>;
       'forge-slider': CustomElementProps<ISliderComponent>;
-      'forge-split-view': CustomElementProps<ISplitViewComponent>;
       'forge-split-view-panel': CustomElementProps<ISplitViewPanelComponent>;
+      'forge-split-view': CustomElementProps<ISplitViewComponent>;
       'forge-stack': CustomElementProps<IStackComponent>;
       'forge-state-layer': CustomElementProps<IStateLayerComponent>;
-      'forge-stepper': CustomElementProps<IStepperComponent>;
       'forge-step': CustomElementProps<IStepComponent>;
+      'forge-stepper': CustomElementProps<IStepperComponent>;
       'forge-switch': CustomElementProps<ISwitchComponent>;
-      'forge-table': CustomElementProps<ITableComponent>;
-      'forge-tab': CustomElementProps<ITabComponent>;
       'forge-tab-bar': CustomElementProps<ITabBarComponent>;
+      'forge-tab': CustomElementProps<ITabComponent>;
+      'forge-table': CustomElementProps<ITableComponent>;
       'forge-text-field': CustomElementProps<ITextFieldComponent>;
       'forge-time-picker': CustomElementProps<ITimePickerComponent>;
       'forge-toast': CustomElementProps<IToastComponent>;
       'forge-toolbar': CustomElementProps<IToolbarComponent>;
       'forge-tooltip': CustomElementProps<ITooltipComponent>;
-      'forge-view': CustomElementProps<IViewComponent>;
       'forge-view-switcher': CustomElementProps<IViewSwitcherComponent>;
+      'forge-view': CustomElementProps<IViewComponent>;
     }
   }
-};
+}
