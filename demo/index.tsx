e7bfbeeafb67178694faf247ba922bc171a52dff
Nichols, Kieran <kieran.nichols@tylertech.com>
2025-10-03 12:06:21 -0400

    chore: revert test code

--- demo/index.tsx
+++ demo/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-// import { defineComponents } from '@tylertech/forge';
-
-// defineComponents();
-import '@tylertech/forge/app-bar';
 
 ReactDOM.render(
   <React.StrictMode>
