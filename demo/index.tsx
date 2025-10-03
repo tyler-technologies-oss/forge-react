6a6f6183b707dfc0e6a789ddf1b93f96551e2885
Nichols, Kieran <kieran.nichols@tylertech.com>
2025-10-03 12:07:12 -0400

    chore: replace missing import

--- demo/index.tsx
+++ demo/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { defineComponents } from '@tylertech/forge';
+
+defineComponents();
 
 ReactDOM.render(
   <React.StrictMode>
